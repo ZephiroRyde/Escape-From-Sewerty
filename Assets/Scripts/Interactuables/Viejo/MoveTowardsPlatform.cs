9c75e7136bc69d3a4618d3416d0cb4e663461e1a
ZephiroRyde <104146932+ZephiroRyde@users.noreply.github.com>
2022-05-26 17:43:18 -0300

    Revert "Merge branch 'main' of https://github.com/ZephiroRyde/Escape-From-Sewerty"
    This reverts commit 9de358a27d057069d70f4e76ea664ce5915b0270, reversing
    changes made to afe9ee37f43d8c1bbbf43c6c42fe3b1fb0b6c074.

--- Assets/Scripts/Interactuables/Viejo/MoveTowardsPlatform.cs
+++ Assets/Scripts/Interactuables/Viejo/MoveTowardsPlatform.cs
@@ -5,55 +5,46 @@ using UnityEngine;
 public class MoveTowardsPlatform : MonoBehaviour
 {
     public GameObject Lever;
-    [SerializeField] private Vector3 _targetPosition;
-    [SerializeField] private float _speed = 10;
-    [SerializeField] private bool _backToOrigin = false;
+    LeverMove leverMove;
+    [SerializeField] Vector3 _targetPosition;
+    [SerializeField] float _speed = 10;
+    private Vector3 _initialPosition;
+    [SerializeField] private bool _back = false;
     [SerializeField] private bool _isMoving = false;
     [SerializeField] private Transform _playerTransform;
-    private Vector3 _initialPosition;
-    LeverMove leverMove;
-
     private void Awake()
     {
         leverMove = Lever.GetComponent<LeverMove>();
         _initialPosition = transform.position;
     }
-
     void Update()
-    {
-        if (_isMoving == true)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, (_backToOrigin == true ? _initialPosition: _targetPosition), _speed * Time.deltaTime);
-            CheckForExtremes();
-        }
-        else
-        {
-            CheckForInput();
-        }
-    }
-
-    private void CheckForInput()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && leverMove.LeverActivation == true)
-        {
-            _isMoving = true;
-        }
-    }
-
-    private void CheckForExtremes()
     {
         if (transform.position == _targetPosition)
         {
-            _backToOrigin = true;
+            _back = true;
             _isMoving = false;
         }
         else if (transform.position == _initialPosition)
         {
-            _backToOrigin = false;
+            _back = false;
             _isMoving = false;
         }
-    }
+        if (Input.GetKeyDown(KeyCode.E) && _isMoving == false && leverMove.LeverActivation == true)
+        {
+            _isMoving = true;
+        }
+        if (_isMoving == true && _back == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
 
+        }
+        else if (_isMoving == true && _back == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _initialPosition, _speed * Time.deltaTime);
+
+        }
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
