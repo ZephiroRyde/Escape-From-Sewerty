d0a6b084ae62117f2f73115e8096099ce1083c34
ZephiroRyde <104146932+ZephiroRyde@users.noreply.github.com>
2022-05-31 10:01:06 -0300

    nuevarata

--- Assets/Scripts/Piero/Enemy/Enemy01Antiguo.cs
+++ Assets/Scripts/Piero/Enemy/Enemy01Antiguo.cs
@@ -3,21 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy01 : MonoBehaviour
+public class Enemy01Antiguo : MonoBehaviour
 {
     public enum EnemyState
     {
         Walking,
-        Falling,
         Death
-
     }
 
 
     [SerializeField] private EnemyState _currentState;
     [SerializeField] private float _rotationSpeed = 15;
     [SerializeField] private float _moveSpeed = 3;
-    [SerializeField] private float _gravitySpeed;
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private bool xAxis = true;
     public bool isGrounded = true;
@@ -31,57 +28,22 @@ public class Enemy01 : MonoBehaviour
     void Update()
     {
 
-        switch(_currentState)
+        if(isGrounded)
         {
-            case EnemyState.Walking:
-                Walking();
-                break;
-
-            case EnemyState.Death:
-                Death();
-                break;
-
-            case EnemyState.Falling:
-                Falling();
-                break;
-        }
-    }
-
-    private void Falling()
-    {
-        _rb.velocity += new Vector3(0, -_gravitySpeed, 0);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
-        if (other.CompareTag("EnemyTargetUbi"))
-        {
-            _moveSpeed = -1 * _moveSpeed;
+            if(xAxis)
+            {
+                _rb.AddForce(new Vector3(_moveSpeed, 0, 0));
+            }
+            else
+            {
+                _rb.AddForce(new Vector3(0, 0, _moveSpeed));
+            }
+            
         }
 
-        if (other.CompareTag("Ground"))
+        if(_currentState == EnemyState.Death)
         {
-            isGrounded = true;
-        }
-
-    }
-    private void Walking()
-    {
-        _rb.velocity = new Vector3(0,0, _moveSpeed);
-
-        if (!isGrounded) 
-        {
-            _currentState = EnemyState.Falling;
-        }
-
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Ground"))
-        {
-            isGrounded = false;
+            Death();
         }
     }
 
@@ -90,4 +52,30 @@ public class Enemy01 : MonoBehaviour
         Destroy(gameObject);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            GameManager.GetInstance.ResetScene();
+        }
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        if (other.CompareTag("EnemyTargetUbi"))
+        {
+            _moveSpeed = -1 * _moveSpeed;
+        }
+        if (other.CompareTag("Spikes"))
+        {
+            _currentState = EnemyState.Death;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 }
