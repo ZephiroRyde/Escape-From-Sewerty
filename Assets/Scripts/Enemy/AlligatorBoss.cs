62a5bbcac01c30bd2011109b393be1d69953b4c0
ZephiroRyde <104146932+ZephiroRyde@users.noreply.github.com>
2022-06-30 00:03:49 -0300

    boss

---
+++ Assets/Scripts/Enemy/AlligatorBoss.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlligatorBoss : MonoBehaviour
+{
+    public enum AlligatorState
+    {
+        Spawn,
+        Run,
+        Attack
+    }
+
+    [Header("States")]
+    [SerializeField] private AlligatorState _currentState = AlligatorState.Spawn;
+
+    [Header("Movement")]
+    [SerializeField] private float _movementSpeed = 8;
+
+    [SerializeField] private Vector3 _initialPos;
+
+    private void Update()
+    {
+        switch(_currentState)
+        {
+
+            case AlligatorState.Spawn:
+                Spawn();
+                break;
+
+            case AlligatorState.Run:
+                Movement();
+                break;
+
+            case AlligatorState.Attack:
+                Attack();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        _currentState = AlligatorState.Spawn;
+    }
+
+    private void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Spawn()
+    {
+        transform.position = _initialPos;
+    }
+
+    public void Movement()
+    {
+
+    }
+}
